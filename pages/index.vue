b30c76218cbbbec10451a9c0aad12266ae203327
Zalbani <pro.pierson.alban@gmail.com>
2020-02-24 18:00:24 +0100

    Image set

--- pages/index.vue
+++ pages/index.vue
@@ -1,57 +1,19 @@
 <template>
   <div class="ftc0-section grid">
-    <div class="item item--large">
-    </div>
-    <div class="item item--medium">
-    </div>
-    <div class="item item--medium">
-    </div>
-    <div class="item item--medium">
-    </div>
-    <div class="item item--medium">
-    </div>
-    <div class="item item--large">
-    </div>
-    <div class="item">
-      <div class="item__details">
-        powder toffee
-      </div>
-    </div>
-    <div class="item item--medium">
-      <div class="item__details">
-        pudding cheesecake
-      </div>
-    </div>
-    <div class="item item--large">
-      <div class="item__details">
-        toffee bear claw
-      </div>
-    </div>
-    <div class="item">
-      <div class="item__details">
-        cake cookie croissant
-      </div>
-    </div>
-    <div class="item item--medium">
-      <div class="item__details">
-        liquorice sweet roll
-      </div>
-    </div>
-    <div class="item item--medium">
-      <div class="item__details">
-        chocolate marzipan
-      </div>
-    </div>
-    <div class="item item--large">
-      <div class="item__details">
-        danish dessert lollipop
-      </div>
-    </div>
-    <div class="item">
-      <div class="item__details">
-        sugar plum dragée
-      </div>
-    </div>
+    <div class="item item--large" />
+    <div class="item item--medium" />
+    <div class="item item--medium" />
+    <div class="item item--medium" />
+    <div class="item item--medium" />
+    <div class="item item--large" />
+    <div class="item" />
+    <div class="item item--medium" />
+    <div class="item item--large" />
+    <div class="item" />
+    <div class="item item--medium" />
+    <div class="item item--medium" />
+    <div class="item item--large" />
+    <div class="item" />
   </div>
 </template>
 <style>
@@ -82,7 +44,6 @@
     background: url("https://images.unsplash.com/photo-1470124182917-cc6e71b22ecc?dpr=2&auto=format&fit=crop&w=1500&h=1000&q=80&cs=tinysrgb&crop=");
     background-size: cover;
     background-position: center;
-    box-shadow: -2px 2px 10px 0px rgba(68, 68, 68, 0.4);
     -webkit-transition: -webkit-transform 0.3s ease-in-out;
     transition: -webkit-transform 0.3s ease-in-out;
     transition: transform 0.3s ease-in-out;
@@ -90,23 +51,23 @@
     cursor: pointer;
     counter-increment: item-counter;
   }
-  .item:after {
-    content: '';
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    background-color: black;
-    opacity: 0.3;
-    -webkit-transition: opacity 0.3s ease-in-out;
-    transition: opacity 0.3s ease-in-out;
-  }
+  /*.item:after {*/
+  /*  content: '';*/
+  /*  position: absolute;*/
+  /*  width: 100%;*/
+  /*  height: 100%;*/
+  /*  background-color: black;*/
+  /*  opacity: 0.3;*/
+  /*  -webkit-transition: opacity 0.3s ease-in-out;*/
+  /*  transition: opacity 0.3s ease-in-out;*/
+  /*}*/
   .item:hover {
     -webkit-transform: scale(1.05);
     transform: scale(1.05);
   }
-  .item:hover:after {
-    opacity: 0;
-  }
+  /*.item:hover:after {*/
+  /*  opacity: 0;*/
+  /*}*/
   .item--medium {
     grid-row-end: span 2;
   }
@@ -121,22 +82,6 @@
       grid-column: 1/-1;
       grid-row-end: span 2;
     }
-  }
-  .item__details {
-    position: relative;
-    z-index: 1;
-    padding: 15px;
-    background: #fff;
-    text-transform: lowercase;
-    letter-spacing: 1px;
-    color: #828282;
-  }
-  .item__details:before {
-    content: counter(item-counter);
-    font-weight: bold;
-    font-size: 1.1rem;
-    padding-right: 0.5em;
-    color: #444;
   }
 </style>
 <script>
